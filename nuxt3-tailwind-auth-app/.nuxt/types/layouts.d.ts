d694f6cd3b334b9fd438de7a5dd033195699cbe8
jask1123 <50744263+jask1123@users.noreply.github.com>
2023-06-14 22:28:40 +0900

    登録

--- nuxt3-tailwind-auth-app/.nuxt/types/layouts.d.ts
+++ nuxt3-tailwind-auth-app/.nuxt/types/layouts.d.ts
@@ -1,6 +1,6 @@
 import { ComputedRef, Ref } from 'vue'
 export type LayoutKey = "default"
-declare module "/Users/user/Documents/fastapi_prototype/nuxt3-tailwind-auth-app/node_modules/nuxt/dist/pages/runtime/composables" {
+declare module "/Users/user/開発/fastapi_proto/fastapi_prototype/nuxt3-tailwind-auth-app/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
   }
