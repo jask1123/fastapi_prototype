3b0455e3e990e2b10bfafdc8cdbbf91f178335eb
jask1123 <50744263+jask1123@users.noreply.github.com>
2023-06-18 03:46:24 +0900

    6/18

--- fastapi/fastapi-mysql-docker-main/api/main.py
+++ fastapi/fastapi-mysql-docker-main/api/main.py
@@ -155,6 +155,6 @@ async def websocket_endpoint(ws: WebSocket):
                     # 接続が切れた場合、当該クライアントを削除する
                     del clients[client]
     except:
-        await ws.close()
+        # await ws.close()
         # 接続が切れた場合、当該クライアントを削除する
         del clients[key]
--- nuxt3-tailwind-auth-app/pages/chat.vue
+++ nuxt3-tailwind-auth-app/pages/chat.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div class="chat-container">
     <h1>WebSocket Chat</h1>
-    <form @submit.prevent="sendMessage">
+    <form @submit.prevent="sendMessage" class="message-form">
       <input type="text" v-model="messageText" autocomplete="off" />
-      <button>Send</button>
+      <button class="send-button">Send</button>
     </form>
-    <ul id="messages">
-      <li v-for="message in messages" :key="message.id">{{ message.content }}</li>
+    <ul id="messages" class="message-list">
+      <li v-for="message in messages" :key="message.id" class="message-item">{{ message.content }}</li>
     </ul>
   </div>
 </template>
@@ -39,5 +39,32 @@ export default {
 </script>
 
 <style>
-/* CSS スタイルを適用する場合はここに記述します */
+.chat-container {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.message-form {
+  display: flex;
+  margin-bottom: 20px;
+}
+
+.message-form input {
+  flex-grow: 1;
+  padding: 5px;
+}
+
+.send-button {
+  margin-left: 10px;
+}
+
+.message-list {
+  list-style-type: none;
+  padding: 0;
+}
+
+.message-item {
+  margin-bottom: 5px;
+}
 </style>
